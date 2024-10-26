82bd0ef9bbe350a9eb18f2e47e02c66d19e77922
AlexxxGorin <alexgorin37@gmail.com>
2024-10-26 22:27:08 +0300

    html fix

--- internal/app/app.py
+++ internal/app/app.py
@@ -65,12 +65,14 @@ html = """
         <script>
             var ws = new WebSocket("ws://localhost:8000/ws");
             ws.onmessage = function(event) {
-                var messages = document.getElementById('messages')
-                var message = document.createElement('li')
-                var content = document.createTextNode(event.data)
-                message.appendChild(content)
-                messages.appendChild(message)
-            };
+            var imageContainer = document.getElementById('image-container');
+            imageContainer.innerHTML = '';
+            var image = document.createElement('img');
+            image.src = event.data;
+            imageContainer.appendChild(image);
+        };
+
+
             function sendMessage(event) {
                 var input = document.getElementById("messageText")
                 ws.send(input.value)

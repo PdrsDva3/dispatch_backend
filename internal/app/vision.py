699449c5a56f44d8e1a9919e99bc7db8b565a7f6
AlexxxGorin <alexgorin37@gmail.com>
2024-10-26 20:27:03 +0300

    implementing YOLO-v11 fine-tuned v1 model

---
+++ internal/app/vision.py
@@ -0,0 +1,62 @@
+import cv2
+from ultralytics import YOLO
+from fastapi import FastAPI, WebSocket, BackgroundTasks, Request
+import asyncio
+import uvicorn
+import base64
+from PIL import Image
+import json
+import io
+
+
+video_path = "/Users/alexgorin/Documents/Development/pythonProject2/itc2024/rzhd2_dataset/2_5208500968638928983.mp4"
+
+# Load the YOLO model
+model = YOLO("/Users/alexgorin/Documents/Development/pythonProject2/itc2024/best.onnx")  # pretrained YOLO11n model
+
+def funct(video_path):
+    # Open the video file
+    cap = cv2.VideoCapture(video_path)
+
+    # Loop through the video frames
+    frame_count = 0
+    while cap.isOpened():
+        frame_count += 1
+        # Read a frame from the video
+        success, frame = cap.read()
+
+        if success:
+
+            # Run YOLO inference on the frame
+            if frame_count % 3 == 0:
+                results = model(frame)
+                big_train_count = 0
+                # print("boxes---")
+                for r in results:
+                    for i in range(len(r.boxes.cls)):
+                        if r.boxes.cls[i] == 4:
+                            # print("coords", r.boxes.xywh[i])
+                            w = r.boxes.xywh[i][2]
+                            h = r.boxes.xywh[i][3]
+                            if w > 200 and h > 300:
+                                big_train_count += 1
+
+                # Visualize the results on the frame
+                annotated_frame = results[0].plot()
+                print("Big trains", big_train_count)
+                # Display the annotated frame
+                cv2.imshow("YOLO Inference", annotated_frame)
+
+                # Break the loop if 'q' is pressed
+                if cv2.waitKey(1) & 0xFF == ord("q"):
+                    break
+        else:
+            # Break the loop if the end of the video is reached
+            break
+
+    # Release the video capture object and close the display window
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+funct(video_path)
